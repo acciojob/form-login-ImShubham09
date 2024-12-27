a8f803df916edb00c0c8e836d77c137cc75cbd14
ImShubham09 <singhashubham612@gmail.com>
2024-12-27 11:56:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function getFormvalue() {
     //Write your code here
+    // Prevent the default form submission behavior
+    event.preventDefault();
 
+    // Get the form element
+    const form = document.getElementById("form1");
+
+    // Retrieve the values of the input fields by their name attribute
+    const firstName = form.elements["fname"].value;
+    const lastName = form.elements["lname"].value;
+
+    // Display the concatenated result using alert
+    alert(firstName + " " + lastName);
 }
+
